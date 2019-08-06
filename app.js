2b4eaf29aca1d0444bd1288901cf4201b9c9a9ec
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-08-06 10:19:05 +0100

    Schemas & Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const grapQlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use(
+  "/graphql",
+  grapQlHttp({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "All night coding "];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
