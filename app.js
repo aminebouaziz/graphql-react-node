f8e5e3eebcb7569c75f2e8f754805fe7bfd560e7
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-08-06 15:09:43 +0100

    date: args.eventInput.date

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const grapQlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -10,11 +13,24 @@ app.use(
   "/graphql",
   grapQlHttp({
     schema: buildSchema(`
+    type Event{
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    input EventInput{
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput:EventInput): Event
     }
 
     schema {
@@ -24,11 +40,18 @@ app.use(
   `),
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "All night coding "];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
